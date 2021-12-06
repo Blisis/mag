ad17be273a8bb00f37d8ea3e09a54ed7ccfb13e6
Blisis <radu.chirita92@gmail.com>
2021-12-07 00:29:46 +0200

    .

--- detalii_produs.php
+++ detalii_produs.php
@@ -7,10 +7,28 @@ $produs=$database->query("
 select * from produse where id={$id}; 
 ")->fetch_all(MYSQLI_ASSOC);
 $produs=reset($produs);
+//var_dump($produs);
+//die();
+/*array(11) {
+["id"]=> string(1) "4"
+ok["nume"]=> string(8) "laptop 2"
+ok["descriere"]=> string(15) "asdfasdfhfgjdgj"
+["pret"]=> string(5) "12.00"
+nu["stoc"]=> string(1) "1"
+["poza"]=> string(22) "1635018567_38083_2.png"
+ok["greutate"]=> string(5) "12.00"
+ok["lungime"]=> string(5) "12.00"
+ok["latime"]=> string(5) "12.00"
+nu["id_categorie"]=> string(1) "1"
+["data"]=> string(19) "2021-12-02 19:50:25" }
+
+
+
+*/
 ?>
 <html>
 <head>
-    <title>Acasa</title>
+    <title><?php echo $produs['nume']; ?></title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="js/app.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha512-Dop/vW3iOtayerlYAqCgkVr2aTr2ErwwTYOvRFUpzl2VhCMJyjQF0Q9TjUXIo6JhuM/3i0vVEt2e/7QQmnHQqw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -34,9 +52,45 @@ $produs=reset($produs);
 <img src="<?php url?>imagini/produse/<?php echo$produs['poza'] ?>" class="img-responsive"  style="">
         </div>
 
-        <div class="col-md-6">
-            decriere
-            si restu
+        <div class="col-md-9">
+            <div class="col-md-3">
+                Descriere:
+            </div>
+            <div class="col-md-9">
+            <?php echo $produs['descriere']; ?>
+            </div>
+            <div class="col-md-3">
+                Greutate:
+            </div>
+            <div class="col-md-9">
+            <?php echo $produs['greutate']; ?>
+            </div>
+            <div class="col-md-3">
+                Lungime:
+            </div>
+            <div class="col-md-9">
+                <?php echo $produs['lungime']; ?>
+            </div>
+            <div class="col-md-3">
+                Latime:
+            </div>
+            <div class="col-md-9">
+                <?php echo $produs['latime']; ?>
+            </div>
+            <div class="col-md-3">
+                A fost adaugat pe :
+            </div>
+            <div class="col-md-9">
+                <?php echo $produs['data']; ?>
+            </div>
+            <div class="col-md-3">
+               Pret :
+            </div>
+            <div class="col-md-9">
+                <?php echo $produs['pret']; ?>
+            </div>
+
+
 
 
 
@@ -45,7 +99,7 @@ $produs=reset($produs);
 
         <br>
         <a href="index.php" class="btn btn-info" role="button">Inapoi</a>
-        <p><a href="#"  class="btn btn-primary adaugareCos" data-id="<?php echo $produs[0]['id'];?>" role="button" >Adauga in cos</a>
+        <p><a href="#"  class="btn btn-primary adaugareCos" data-id="<?php echo $produs['id'];?>" role="button" >Adauga in cos</a>
         </div>
 
 
